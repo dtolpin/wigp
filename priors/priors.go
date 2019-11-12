072e6d4f8742ad70f9ddf061858c0b6708342f8a
David Tolpin <david.tolpin@gmail.com>
2019-11-12 02:18:56 +0200

    enough fiddling with the priors

--- priors/priors.go
+++ priors/priors.go
@@ -26,14 +26,14 @@ func (m *Priors) Observe(x []float64) float64 {
 	// Output scale is mostly less than 1.
 	ll += Normal.Logp(-1, 1, x[c])
 	// Length scale is around 1, in wide margins.
-	ll += Normal.Logp(0, 2, x[l])
+	ll += Normal.Logp(0, 5, x[l])
 	// The noise is scaled by 0.01 in the kernel.
-	ll += Normal.Logp(0, 1, x[s])
+	ll += Normal.Logp(0, 5, x[s])
 
 	// Priors of the renewal process
 	// -----------------------------
 	//  We allow inputs to move slightly.
-	ll += Normal.Logps(0, 0.25, x[i0:]...)
+	ll += Normal.Logps(0, 0.5, x[i0:]...)
 
 	return ll
 }
